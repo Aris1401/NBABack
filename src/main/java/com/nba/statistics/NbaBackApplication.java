df254c1621a93d2dde169819072662217b7ae9d5
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2023-11-22 05:39:01 +0300

    Update NbaBackApplication.java

--- src/main/java/com/nba/statistics/NbaBackApplication.java
+++ src/main/java/com/nba/statistics/NbaBackApplication.java
@@ -2,8 +2,14 @@ package com.nba.statistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.nba.statistics.*")
+@EnableJpaRepositories(basePackages = "com.nba.statistics.repository")
+@EntityScan(basePackages = "com.nba.statistics.model")
 public class NbaBackApplication {
 
 	public static void main(String[] args) {
